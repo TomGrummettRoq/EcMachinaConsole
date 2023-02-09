921571b3b169224a82cc12baf4cb640136c3e3e5
Tom Grummmett <tom.grummett@roq.co.uk>
2023-02-09 10:41:39 +0000

    Joke Loop

---
+++ index.js
@@ -0,0 +1,21 @@
+
+const prompt = require('prompt-sync')();
+let input = '';
+
+let record = [];
+
+while (input != 'quit') {
+    input = prompt('Would you like to hear a joke? ')
+    let command = input.toUpperCase();
+
+    if (input == 'yes') {
+        prompt('Why did the chicken cross the road? ');        
+    }
+
+    else (input != 'yes'); {
+        input = record.push();
+    console.log('Boring... ')
+    };
+
+
+}
